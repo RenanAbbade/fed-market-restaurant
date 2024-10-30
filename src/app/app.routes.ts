cca697ce219eef8fdd64ae0964c522c3eee87418
RenanAbbade <renanabbade1997@gmail.com>
2024-10-29 22:32:40 -0300

    lazy loading no routes, e route-outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProdutosComponent } from './cardapio/produtos/produtos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'cardapio', pathMatch: 'full' },
+  { path: 'cardapio', loadChildren: () => import('./cardapio/cardapio.module').then(m => m.CardapioModule) }
+  //lazy loading para melhor desempenho, carregando somente o módulo utilizado a partir do path, facilitando a escalabilidade da aplicação, adiciona novos módulos sem gerar
+  //impacto no desempenho
+
+];

07cd671c04b44b64e31dacf2d0716e26888b0bf0
RenanAbbade <renanabbade1997@gmail.com>
2024-10-29 23:48:37 -0300

    menu

--- src/app/cardapio/produtos/produtos.component.ts
+++ src/app/cardapio/produtos/produtos.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-produtos',
   standalone: true,
-  imports: [],
+  imports: [    MatCardModule,
+    MatButtonModule,],
   templateUrl: './produtos.component.html',
   styleUrl: './produtos.component.scss'
 })
